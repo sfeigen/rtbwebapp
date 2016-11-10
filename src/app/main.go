f65138ce62d55d5094b4e92925007f2cc330d8b9
Sam Feigenbaum <sfeigen@live.com>
2016-11-10 18:08:16 -0500

    Loaded up go mvc

--- src/app/main.go
+++ src/app/main.go
@@ -1,17 +1,67 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"log"
+	"os"
+	"runtime"
+
+	"encoding/json"
+	"vendor/shared/database"
+	"vendor/shared/email"
+	"vendor/shared/recaptcha"
+	"vendor/shared/server"
+	"vendor/shared/session"
+	"vendor/shared/view"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "I am a GO application running on Google Cloud.")
+func init() {
+	// Verbose logging with filename and ln
+	log.SetFlags(log.Lshortfile)
 
+	// Use all cores
+	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 func main() {
-	fmt.Println("Basic web server is starting on port 8080...")
-	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8080", nil)
+	// Load the configuration file
+	jsonconfig.Load("config"+string(os.PathSeparator)+"config.json", config)
+
+	// Configure the session cookie store
+	session.Configure(config.Session)
+
+	// Connect to database
+	database.Connect(config.Database)
+
+	// Configure the Google reCAPTCHA prior to loading view plugins
+	recaptcha.Configure(config.Recaptcha)
+
+	// Setup the views
+	view.Configure(config.View)
+	view.LoadTemplates(config.Template.Root, config.Template.Children)
+	view.LoadPlugins(
+		plugin.TagHelper(config.View),
+		plugin.NoEscape(),
+		plugin.PrettyTime(),
+		recaptcha.Plugin())
+
+	// Start the listener
+	server.Run(route.LoadHTTP(), route.LoadHTTPS(), config.Server)
+}
+
+// config settings variable
+var config = &configuration{}
+
+// cofiguration struct
+type configuration struct {
+	Database  database.Info   `json:"Database"`
+	Email     email.SMTPInfo  `json:"Email"`
+	Recaptcha recaptcha.Info  `json:"Recaptcha"`
+	Server    server.Server   `json:"Server"`
+	Session   session.Session `json:"Session"`
+	Template  view.Template   `json:"Template"`
+	View      view.View       `json:"View"`
+}
+
+func (c *configuration) ParseJSON(b []byte) error {
+	return json.Unmarshal(b, &c)
 }
